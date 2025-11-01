38ce69127c6a386f90033c633cf798e01a95c079
LucasEPO <lucas.epoliveira@gmail.com>
2025-11-01 15:31:20 -0300

    Add categories banners

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import heroImage from '@/public/assets/heroImage.png';
 import Highlight from "@/components/ui/Highlight";
+import CategoryBanner from "@/components/ui/CategoryBanner";
 
 export default function Home() {
   return (
@@ -16,13 +17,20 @@ export default function Home() {
       </div> 
     </section>
     <section className="flex flex-col text-center p-5 my-4 items-center">
-      <h2 className="mb-4 text-white font-bold">Destaques</h2>
+      <h2 className="mb-4 text-black font-bold">Destaques</h2>
       <div className="w-[90%] flex justify-center items-center gap-8 flex-wrap">
         <Highlight />
         <Highlight />
         <Highlight />
         <Highlight />
         <Highlight />
+      </div>
+    </section>
+    <section className="my-2 flex flex-col text-center p-5 items-center">
+      <h2 className="text-4xl text-[hex(#f8fafc)] tracking-[3px] mb-3 uppercase font-extrabold [text-shadow:0_2px_10px_rgba(0,0,0,0.5)]">Categorias</h2>
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] w-[90%] h-[200px] gap-8">
+        <CategoryBanner />
+        <CategoryBanner />
       </div>
     </section>
     </>

58a60c11f8c940f552d057f2e4b41582a81143aa
LucasEPO <lucas.epoliveira@gmail.com>
2025-11-30 23:06:37 -0300

    Fix products page for vercel

--- app/(pages)/products/page.tsx
+++ app/(pages)/products/page.tsx
@@ -2,6 +2,7 @@ import { categoryService } from "@/services/categoryService";
 import { collectionService } from "@/services/collectionService";
 import { productService } from "@/services/productService";
 import ProductsClientPage from "./_components/ProductsClient";
+import { Suspense } from "react";
 
 export default async function Page() {
   const [categories, collections, products] = await Promise.all([
@@ -11,10 +12,12 @@ export default async function Page() {
   ]);
 
   return (
-    <ProductsClientPage
-      categories={categories}
-      collections={collections}
-      products={products}
-    />
+    <Suspense fallback={<div>Carregando...</div>}>
+      <ProductsClientPage
+        categories={categories}
+        collections={collections}
+        products={products}
+      />
+    </Suspense>
   );
 }
